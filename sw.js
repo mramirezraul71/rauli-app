ee82a96da8b723ebe4ede1a1e01e8715be4e8dd6
mramirezraul71 <mramirezraul71@gmail.com>
2026-01-03 16:51:18 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "rauli-cache-v1";
+const ASSETS = [
+  "./",
+  "./rauli.html",
+  "./manifest.json",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).catch(() => caches.match("./rauli.html")))
+  );
+});
